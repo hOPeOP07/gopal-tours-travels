abcee449cd0c456a845dec0f3a15b21066fa5d8a
Shikhar Chaturvedi <shikhar.pilu@gmail.com>
2026-08-12 21:56:23 +0530

    Add website setting types

---
+++ types/website.ts
@@ -0,0 +1,23 @@
+export interface SiteSettings {
+  companyName: string;
+  logoUrl: string;
+  tagline: string;
+  phone: string;
+  email: string;
+  whatsapp: string;
+  address: string;
+}
+
+export interface HeroSettings {
+  heading: string;
+  subheading: string;
+  backgroundImageUrl: string;
+  primaryButtonText: string;
+  secondaryButtonText: string;
+}
+
+export interface SocialLinks {
+  instagram: string;
+  facebook: string;
+  youtube: string;
+}
